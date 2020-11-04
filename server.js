c3613dc3923b71b4cd9394c33adb6c3dd1a49759
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2020-11-03 20:18:54 -0600

    added user model and user route to register

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect Datebase
 connectDB();
 
+// Init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API RUNNING'));
 
 // Define Routes
@@ -13,6 +16,7 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
