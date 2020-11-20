40ee9b8edfb41018b20710c7e280f77e482a94a8
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2020-11-19 23:56:06 -0600

    added comments about alerts

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -3,12 +3,13 @@ import { SET_ALERT, REMOVE_ALERT } from './types';
 
 // Thunk middleware lets us do the double arrow
 export const setAlert = (msg, alertType, timeout = 5000) => (dispatch) => {
+  // sets a random id for each alert
   const id = uuidv4();
 
   dispatch({
     type: SET_ALERT,
     payload: { msg, alertType, id },
   });
-
+  // Logs out a user if they are logged in too long
   setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
 };
