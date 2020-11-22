281a99ae010d5cc789f2db3f4832f6ce4cb705b0
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2020-11-22 17:25:49 -0600

    register and authenticate

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -8,35 +8,45 @@ import Alert from './components/layout/Alert';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
 
-const App = () => (
-  // Redux Provider
-  <Provider store={store}>
-    {/* Displays different pages in our SPA */}
-    <Router>
-      {/* Fragment lets us return multiple elements */}
-      <Fragment>
-        {/* The Navbar from components,  This will always display */}
-        <Navbar />
-        {/* Landing page route */}
-        <Route exact path='/' component={Landing} />
-        {/* This section puts all of our other pages in a container */}
-        <section className='container'>
-          {/* Our Alert for errors, client side */}
-          <Alert />
-          {/* Switch lets us change what page is displayed */}
-          <Switch>
-            {/* Register page route */}
-            <Route exact path='/register' component={Register} />
-            {/* Login page route */}
-            <Route exact path='/login' component={Login} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
-  </Provider>
-);
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+  return (
+    // Redux Provider
+    <Provider store={store}>
+      {/* Displays different pages in our SPA */}
+      <Router>
+        {/* Fragment lets us return multiple elements */}
+        <Fragment>
+          {/* The Navbar from components,  This will always display */}
+          <Navbar />
+          {/* Landing page route */}
+          <Route exact path='/' component={Landing} />
+          {/* This section puts all of our other pages in a container */}
+          <section className='container'>
+            {/* Our Alert for errors, client side */}
+            <Alert />
+            {/* Switch lets us change what page is displayed */}
+            <Switch>
+              {/* Register page route */}
+              <Route exact path='/register' component={Register} />
+              {/* Login page route */}
+              <Route exact path='/login' component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 export default App;
