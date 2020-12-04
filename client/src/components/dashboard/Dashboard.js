e1f28b7fafd8f83be8d1b6a931623edbcb3d67e8
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2020-12-03 21:57:24 -0600

    experience, education, profile, and user can now be deleted

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { getCurrentProfile } from '../../actions/profile';
+import { getCurrentProfile, deleteAccount } from '../../actions/profile';
 import DashboardActions from './DashboardActions';
 import Experience from './Experience';
 import Education from './Education';
@@ -10,6 +10,7 @@ import { Link } from 'react-router-dom';
 
 const Dashboard = ({
   getCurrentProfile,
+  deleteAccount,
   auth: { user },
   profile: { profile, loading },
 }) => {
@@ -30,6 +31,11 @@ const Dashboard = ({
           <DashboardActions />
           <Experience experience={profile.experience} />
           <Education education={profile.education} />
+          <div className='my-2'>
+            <button className='btn btn-danger' onClick={() => deleteAccount()}>
+              <i className='fas fa-user-minus'></i> Delete My Account
+            </button>
+          </div>
         </Fragment>
       ) : (
         <Fragment>
@@ -45,6 +51,7 @@ const Dashboard = ({
 
 Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
+  deleteAccount: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   profile: PropTypes.object.isRequired,
 };
@@ -54,4 +61,6 @@ const mapStatetoProps = (state) => ({
   profile: state.profile,
 });
 
-export default connect(mapStatetoProps, { getCurrentProfile })(Dashboard);
+export default connect(mapStatetoProps, { getCurrentProfile, deleteAccount })(
+  Dashboard
+);
