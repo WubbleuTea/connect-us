258334a89272aa396ae29a2525f5f3963e83a784
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2020-12-05 11:00:41 -0600

    page to show all developers completed

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -4,7 +4,7 @@ import {
   GET_PROFILES,
   GET_REPOS,
   UPDATE_PROFILE,
-  PROFILE_ERROR,
+  PROFILE_ERROR
 } from '../actions/types';
 
 const initialState = {
@@ -12,7 +12,7 @@ const initialState = {
   profiles: [],
   repos: [],
   loading: true,
-  error: {},
+  error: {}
 };
 
 export default function profileSwitch(state = initialState, action) {
@@ -24,32 +24,32 @@ export default function profileSwitch(state = initialState, action) {
       return {
         ...state,
         profile: payload,
-        loading: false,
+        loading: false
       };
     case GET_PROFILES:
       return {
         ...state,
         profiles: payload,
-        loading: false,
+        loading: false
       };
     case PROFILE_ERROR:
       return {
         ...state,
         error: payload,
-        loading: false,
+        loading: false
       };
     case CLEAR_PROFILE:
       return {
         ...state,
         profile: null,
         repos: [],
-        loading: false,
+        loading: false
       };
     case GET_REPOS:
       return {
         ...state,
         repos: payload,
-        loading: false,
+        loading: false
       };
     default:
       return state;
