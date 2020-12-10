fca53fd7212cbf3b3e1056fa6bffca1deade03e4
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2020-12-09 20:53:49 -0600

    started posts to view many posts.

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import AddEducation from './components/profile-forms/AddEducation';
 import EditProfile from './components/profile-forms/EditProfile';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
@@ -75,6 +76,7 @@ const App = () => {
                 path='/add-education'
                 component={AddEducation}
               />
+              <PrivateRoute exact path='/posts' component={Posts} />
             </Switch>
           </section>
         </Fragment>
